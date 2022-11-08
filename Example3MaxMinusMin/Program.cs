867a9c925dd00a7a2e1b14f574241aedcc67e31f
Sergey <melnikov_s_a@mail.ru>
2022-11-09 01:59:00 +0300

    DoubleArray unfinished

---
+++ Example3MaxMinusMin/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double[] someNum = new double[5];
+someNum = RandomFill(someNum);
+// OddSum(someNum);
+
+double[] RandomFill(double[] numbers)
+{
+    Console.WriteLine("Массив таков");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Random().Next();
+        Console.Write($"{numbers[i]} . ");
+    }
+    Console.WriteLine();
+    return numbers;
+}
+
+
+// void OddSum(double[] numbers)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         if (i % 2 != 0)
+//         {
+//             sum = sum + numbers[i];
+//         }
+//     }
+//     Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+// }
